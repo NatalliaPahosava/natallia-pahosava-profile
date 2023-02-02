4edf069fe4830b06dc1a6f32811f5c640060f0c6
NatalliaPahosava <mulato4ka114@gmail.com>
2023-02-02 13:05:19 -0500

    added project page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,21 +1,32 @@
+import { useParams } from 'react-router-dom'
 import BtnGitHub from '../components/btnGitHub/BtnGitHub'
 import img from './../img/projects/project1.png'
-
+import projects from './../helpers/projectsList'
 
 const Project = () => {
+  const { id } = useParams()
+  const project = projects[id]
+
   return (
     <main className='section'>
       <div className='container'>
         <div className='project-details'>
-          <h1 className='title-1'>NextLife</h1>
-
+          <h1 className='title-1'>{project.title}</h1>
           <img src={img} alt='' className='project-details__cover' />
 
           <div className='project-details__desc'>
-            <p>Skills: React, Node.js, MongoDB</p>
+            <a href={project.link} style={{ color: 'red' }}>
+              Demo
+            </a>
+            <p>
+              Skills
+              <br />
+              {project.skills}
+            </p>
           </div>
 
-         <BtnGitHub link="http://github.com"/>
+          {project.gitHubLink &&(
+            <BtnGitHub link={project.gitHubLink} />)}  
         </div>
       </div>
     </main>
